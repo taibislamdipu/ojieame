fa0e275af5f57c7e215056152e213dcc60059376
taibislamdipu <mailtaibislam@gmail.com>
2022-07-15 04:25:47 +0600

    new

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      test: "#FF0000",
+      white: "#ffffff",
+      lightGray: "#999",
+      blue: "#689af8",
+      
+    },
+  },
+  plugins: [],
+};
