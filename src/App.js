b9bba96a223545d6ebada5c7e701c4b9424d4561
taibislamdipu <mailtaibislam@gmail.com>
2022-07-16 04:26:32 +0600

    new

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MySkillSet from "./components/MySkillSet/MySkillSet";
 import NavBar from "./components/NavBar/NavBar";
 import { useMediaQuery } from "react-responsive";
 import Process from "./components/Process/Process";
+import RecentProject from "./components/RecentProject/RecentProject";
 
 function App() {
   const isSmallScreen = useMediaQuery({ query: "(max-width: 768px)" });
@@ -12,6 +13,7 @@ function App() {
       <HeroSection isSmallScreen={isSmallScreen} />
       <MySkillSet isSmallScreen={isSmallScreen} />
       <Process isSmallScreen={isSmallScreen} />
+      <RecentProject />
     </div>
   );
 }
---
+++ src/components/RecentProject/RecentProject.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecentProject = () => {
+  return <div></div>;
+};
+
+export default RecentProject;
